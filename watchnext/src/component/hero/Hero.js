4f31e8bfc6730acee2613427412815c9b4379481
Masasa <73863365+ThapeloMasasa@users.noreply.github.com>
2024-01-06 07:06:05 +0000

    further formatting

--- watchnext/src/component/hero/Hero.js
+++ watchnext/src/component/hero/Hero.js
@@ -5,15 +5,11 @@ import './Hero.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
 import {Link, useNavigate} from 'react-router-dom';
-import Button from 'react-bootstrap/esm/Button';
+
 
 
 export const Hero = ({movies}) => {
-    const navigate = useNavigate()
-
-    function reviews(movieId){
-        navigate(`/Reviews/${movieId}`);
-    }
+   
     console.log('Here are the movies', movies)
   return (
     <div className='movie-carousel-container'>
@@ -34,11 +30,6 @@ export const Hero = ({movies}) => {
                                                     <FontAwesomeIcon className='play-button-icon' icon= {faCirclePlay}/>
                                             </div>
                                             </Link>
-                                        </div>
-                                        <div className='movie-buttons-container'>
-                                            <div className='movie-review-button-container'>
-                                                <Button variant= "info" onClick={()=> reviews(movie.imdbId)}>Reviews</Button>
-                                            </div>
                                         </div>
                                     </div>
                                 </div>
--- watchnext/src/component/movieCard/MovieCard.js
+++ watchnext/src/component/movieCard/MovieCard.js
@@ -1,15 +1,22 @@
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 import './MovieCard.css';
 import Slideshow from '../slideshow/SlideShow';
+import Button from 'react-bootstrap/esm/Button';
 
 const MovieCard = ({movie})=>{
-    
+    const navigate = useNavigate()
+
+    function reviews(movieId){
+        navigate(`/Reviews/${movieId}`);
+    }
     return(
-                <Link className="link" to="/">
+          
                 <div className="card">
+                      <Link className="link" to={`/Trailer/${movie.trailerLink.substring(movie.trailerLink.length - 11)}`}>
                         <div className="card-image">
                            <Slideshow imageUrls={movie.backdrops} />
                         </div>
+                        </Link>
                         <div className="card-content">
                             <h2>{movie.title}</h2>
                             <div className='genres'>
@@ -20,12 +27,20 @@ const MovieCard = ({movie})=>{
                                 ))}
                             
                             </div>
-                            <div>
-                                <p>{movie.releaseDate}</p>
+                           
+                            <div className='reviews'>
+                                <div>{movie.releaseDate}</div>
+                                <div className='movie-review-button-container'>
+                                <Button variant= "info" onClick={()=> reviews(movie.imdbId)}>Reviews</Button>   
+                                </div>
                             </div>
-                        </div>
+                           
+                         
+                                        
+                         </div>
                 </div>
-                </Link>
+               
+               
         
         
     )
